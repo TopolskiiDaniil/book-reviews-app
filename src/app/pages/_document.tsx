1ee7415b702e44eefa2cb07e315983732d6723cb
Даниил Топ <cool.topolskiy.gmail.com>
2025-04-16 12:10:46 +0300

    Initial commit: setup with Next.js, TypeScript, Emotion, ESLint

---
+++ src/app/pages/_document.tsx
@@ -0,0 +1,45 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import createEmotionServer from '@emotion/server/create-instance'
+import createCache from '@emotion/cache'
+import { AppType } from 'next/app';
+
+const cache = createCache({ key: 'css' });
+const { extractCriticalToChunks, constructStyleTagsFromChunks } = createEmotionServer(cache);
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const originalRenderPage = ctx.renderPage
+
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App: AppType) => (props) =>
+          <App emotionCache={cache} {...props} />,
+      })
+
+    const initialProps = await Document.getInitialProps(ctx)
+    const emotionStyles = extractCriticalToChunks(initialProps.html)
+    const emotionStyleTags = constructStyleTagsFromChunks(emotionStyles)
+
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <div dangerouslySetInnerHTML={{ __html: emotionStyleTags }} />
+        </>
+      ),
+    }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
